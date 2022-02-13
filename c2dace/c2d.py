154d6b038ef3511905a6cd5ca06e95e7f69f4f5f
Alexandru Calotoiu <acalotoiu@ethz.ch>
2022-02-13 21:34:29 +0100

    cli

--- c2dace/c2d.py
+++ c2dace/c2d.py
@@ -82,90 +82,19 @@ def execute_command(command):
 if __name__ == "__main__":
 
     args = get_cli_arguments()
-    filecores = [
-        #"spmv",
-        #"2mm",
-        #"sparselu",
-        #"2mm",
-        "3mm",
-        #"adi",
-        #"atax",
-        #"bicg",
-        #"cholesky",  #issue out degree
-        #"correlation",
-        #"covariance",
-        #"deriche",
-        #"doitgen",
-        #"durbin",  #issue n not defined
-        #"fdtd2d",
-        #"floyd_warshall",  #issue, infer expr type
-        #"gemm",
-        #"gemver",
-        #"gesummv",
-        #"gramschmidt",
-        #"heat",
-        #"lu",  #issue out degree
-        #"jacobi_1d",  #issue out degree
-        #"jacobi_2d",
-        #"gemm",
-        #"ludcmp",  #issue out degree
-        #"mvt",
-        #"nussinov",  #broken
-        #"seidel",
-        #"symm",  #issue out degree
-        #"syr2k",
-        # "syrk",
-        # "trisolv",  #        -topological
-        # "trmm",  #issue  out degree
-    ]
-    #    filecores2 = [
-    #"2mm",
-    #"3mm",
-    #"adi",
-    #"atax",
-    #"bicg",
-    #"cholesky",  #issue out degree
-    #"correlation",
-    #"covariance",
-    ##"deriche",
-    #"doitgen",
-    #"durbin",  #issue n not defined
-    #"fdtd2d",
-    #"floyd_warshall",  #issue, infer expr type
-    #"gemm",
-    #"gemver",
-    #"gesummv",
-    #"gramschmidt",
-    #"heat",
-    #"lu",  #issue out degree
-    #"jacobi_1d",  #issue out degree
-    #"jacobi_2d",
-    #"gemm",
-    #"ludcmp",  #issue out degree
-    #"mvt",
-    #"nussinov",  #issue infer expr type
-    #"seidel",
-    #"symm",  #issue out degree
-    #"syr2k",
-    #"syrk",
-    #"trisolv",  #        -topological
-    #"trmm",  #issue  out degree
-    #]
+
     autoopt = False
 
     filecore = os.path.splitext(os.path.basename(args.f))[0]
     fileextension = os.path.splitext(os.path.basename(args.f))[1]
 
-    fileextension = ".c"
-
     _dir = os.path.dirname(args.f)
 
-    for filecore in filecores:
-        filename = _dir + "/" + filecore + fileextension
+    filename = _dir + "/" + filecore + fileextension
 
-        c2d_workflow(_dir,
-                     filecore,
-                     fileextension,
-                     autoopt=False,
-                     verbose=False,
-                     print_ast=False)
+    c2d_workflow(_dir,
+                 filecore,
+                 fileextension,
+                 autoopt=False,
+                 verbose=False,
+                 print_ast=False)
